83cc19df30b34cee1abba5650296def1d6c55367
TaylorTayTyT <taylornguyen904@gmail.com>
2023-03-14 12:21:59 -0400

    button stuff

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,17 @@
 import "./Header.css"
 import { Button, Input } from "@mui/material"
-//import queryString from 'query-string'
+import queryString from 'query-string'
 
 function Header() {
+    const submitAuthorization = (e) => {
+        const client_id = document.getElementById("client_id_field").value
+        const client_secret = document.getElementById("client_secret_field").value
+        const scope = 'user-read-private user-read-email';
+        const url = {
+            "response_type": "code",
+        }
+        console.log(url)
+    }
 
     return (
         <div id="title">
@@ -10,7 +19,7 @@ function Header() {
             <div>
                 <Input id="client_id_field" type="text" placeholder="client_id">hi</Input>
                 <Input id="client_secret_field" type="text" placeholder="client_secret">hi</Input>
-                <Button type="submit" >Submit</Button>
+                <Button type="submit" onClick={submitAuthorization}>Submit</Button>
             </div>
 
         </div>
