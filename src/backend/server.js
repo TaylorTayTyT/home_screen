eea8ca7ffff8ecb51e68e59b2e84c295645a2867
TaylorTayTyT <taylornguyen904@gmail.com>
2023-04-02 17:14:27 -0400

    i actually an get to spotify

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,46 +1,18 @@
-
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
-
-var express = require('express'); // Express web server framework
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-//var proxy = require('http-proxy-middleware')
+const PORT = process.env.PORT || 8888;
+const application = express();
 
-var cors = require("cors")
-var app = express();
-var bodyParser = require('body-parser');
-
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"]
-}))
-
-// configure the app to use bodyParser()
-app.use(bodyParser.urlencoded({
-  extended: true
+application.use(express.json());
+application.use(express.urlencoded({ extended: true}));
+application.use(cors({
+  origin: "*",
 }));
-app.use(bodyParser.json());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const AuthRoutes = require('./routes/AuthRoutes.js');
+application.use('/api', cors(), AuthRoutes);
 
-app.get('/api', cors(), (req, res, next) => {
-  console.log("hi")
-  res.send({ message: "Hello from server!" });
-})
-
-app.post('/api', cors(), (req, res, next) => {
-  console.log(req.body);
-  res.json({ message: "hello from anyone" })
-})
-
-app.listen(8888, () => {
-  console.log("listening to port 8888")
-})
+application.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
