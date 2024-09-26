8be500ffdf3615dfe259f5c1071c4164461955f1
Taylor Nguyen <taylornguyen904@gmail.com>
2024-09-26 15:43:27 -0700

    started carousel dvpmt

--- Components/ArtistPopup.jsx
+++ Components/ArtistPopup.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import "../src/Styles/ArtistPopup.css";
 import "../src/Styles/Font.css";
 import { CloseButton } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 export default function ArtistPopup(props) {
     const [topTracks, SetTopTracks] = useState([]);
-    const [relatedArtists, SetRelatedArtists] = useState([]); 
+    const [relatedArtists, SetRelatedArtists] = useState([]);
     const trackLink = (track) => {
         window.open(track.uri);
     };
@@ -16,49 +17,67 @@ export default function ArtistPopup(props) {
         SetTopTracks([]);
         props.SetPopup(null);
     };
-    useEffect(()=>{
-        if(!props.user || !props.popup) return;
+    useEffect(() => {
+        if (!props.user || !props.popup) return;
         props.user.getArtistTopTracks(props.popup.id)
-        .then(data=>{
-            SetTopTracks(data);
-        });
+            .then(data => {
+                SetTopTracks(data);
+            });
         props.user.getArtistRelatedArtists(props.popup.id)
-        .then(data=>{
-            SetRelatedArtists(data);
+            .then(data => {
+                SetRelatedArtists(data);
+            });
+        document.querySelectorAll(".carousel-indicators").forEach(item=>{
+            item.style.marginBottom = 0; 
+        });
+        document.querySelectorAll("carousel-control-prev").forEach(item=>{
+            item.style.opacity = 1; 
+        });
+        document.querySelectorAll("carousel-control-next").forEach(item=>{
+            item.style.opacity = 1; 
         });
     }, [props.popup]);
 
-    if(!props.popup) return; 
-    return(
+    if (!props.popup) return;
+    return (
         <div className="popupContainer">
             <div className="artistHeader">
-            <div className="popupHeader koulen-regular">{props.popup.name}</div>
-            <div className="nanum-myeongjo-regular"><i>{props.popup.genres.join(", ")}</i></div>
+                <div className="popupHeader koulen-regular">{props.popup.name}</div>
+                <div className="nanum-myeongjo-regular"><i>{props.popup.genres.join(", ")}</i></div>
             </div>
-            <CloseButton onClick={closePopup} className="closeButton"/>
+            <CloseButton onClick={closePopup} className="closeButton" />
 
             <div className="popupHeader koulen-regular">
                 Top Tracks
-                <div className="topTracks">
-                    {topTracks.tracks ? topTracks.tracks.map((track, idx) =>{
-                        if(idx < 4) {
-                            return(
-                                <div onClick={()=> trackLink(track)} className="track" style={{backgroundImage: track.album.images ? `url(${track.album.images[2].url})` : null}}> 
-                                    {track.name}
-                                </div>
-                            )
-                        }
-                    }) : ""}
-                </div>
+                <Carousel controls={true} variant="dark">
+                    <Carousel.Item>
+                            <div className="topTracks">
+                                {topTracks.tracks ? topTracks.tracks.map((track, idx) => {
+                                    if (idx < 4) {
+                                        return (
+                                            <div onClick={() => trackLink(track)} className="track" style={{ backgroundImage: track.album.images ? `url(${track.album.images[2].url})` : null }}>
+                                                {track.name}
+                                            </div>
+                                        )
+                                    }
+                                }) : ""}
+                            </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div>
+                            hi
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
             </div>
 
             <div className="popupHeader koulen-regular">
                 Related Artists
-                <div className="topTracks"> 
-                    {relatedArtists.artists ? relatedArtists.artists.map((artist, idx) =>{
-                        if(idx < 4){
-                            return(
-                                <div onClick={()=>artistLink(artist)} className="track" style={{backgroundImage: artist.images ? `url(${artist.images[2].url})` : null}}>
+                <div className="topTracks">
+                    {relatedArtists.artists ? relatedArtists.artists.map((artist, idx) => {
+                        if (idx < 4) {
+                            return (
+                                <div onClick={() => artistLink(artist)} className="track" style={{ backgroundImage: artist.images ? `url(${artist.images[2].url})` : null }}>
                                     {artist.name}
                                 </div>
                             )
